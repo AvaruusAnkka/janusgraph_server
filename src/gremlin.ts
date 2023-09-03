901668999322ec99836cc3f011f8e27024ba0d9d
Andy <60817686+AvaruusAnkka@users.noreply.github.com>
2023-09-03 13:01:54 +0300

    Functions wrapped in try catches (#4)

--- src/gremlin.ts
+++ src/gremlin.ts
@@ -4,59 +4,102 @@ import 'dotenv/config'
 const traversal = gremlin.process.AnonymousTraversalSource.traversal
 const DriverRemoteConnection = gremlin.driver.DriverRemoteConnection
 
-const url = process.env.GREMLIN_URL ? String(process.env.GREMLIN_URL) : 'ws://localhost:8182/gremlin'
+const url = process.env.GREMLIN_URL
+  ? String(process.env.GREMLIN_URL)
+  : 'ws://localhost:8182/gremlin'
 
 const connection = new DriverRemoteConnection(url)
 
 const g = traversal().withRemote(connection)
 const client = new gremlin.driver.Client(url)
 
-class VertexQueries {
-  #ignoredKeys = new RegExp(/^(id|label)$/, 'i')
+class Utils {
+  submit = async (query: string) => {
+    try {
+      console.log('Query:', query)
+      const response = await client.submit(`g.${query}`)
+      return response
+    } catch (error) {
+      console.error('Error executing Gremlin query:', error)
+      return { error: 'Something went wrong.' }
+    }
+  }
 
-  #createAddQuery = (vertex: { [key: string]: string | number }) => {
-    const entries = Object.entries(vertex)
+  searchQuery = (properties: { [key: string]: string }) => {
+    const entries = Object.entries(properties)
     const addQuery = entries.map(([key, value]) => {
-      if (this.#ignoredKeys.test(key)) return ''
-      else if (typeof value === 'string')
-        return `.property('${key}','${value}')`
-      else if (typeof value === 'number') return `.property('${key}',${value})`
+      if (key === 'id') return `.hasId(${value})`
+      else if (key === 'label') return `.hasLabel('${value}')`
+      else if (Number(value)) return `.has('${key}', ${value})`
+      else return `.has('${key}', '${value}')`
     })
     return addQuery.join('')
   }
 
-  check = (vertexId: number) => g.V(vertexId).hasNext()
+  #ignoredKeys = new RegExp(/^(id|label|to|from)$/, 'i')
 
-  get = (id: number) => g.V(id).elementMap().next()
-
-  add = (vertex: { [key: string]: string | number }) => {
-    const addQuery = this.#createAddQuery(vertex)
-    const query = `addV('${vertex.label}')${addQuery}.next()`
-    return client.submit(`g.${query}`)
+  createAddQuery = (properties: { [key: string]: string }) => {
+    const entries = Object.entries(properties)
+    const addQuery = entries.map(([key, value]) => {
+      if (this.#ignoredKeys.test(key)) return ''
+      else if (Number(value)) return `.property('${key}',${value})`
+      else return `.property('${key}','${value}')`
+    })
+    return addQuery.join('')
   }
-
-  update = (vertex: { [key: string]: string | number }) => {
-    const addQuery = this.#createAddQuery(vertex)
-    const query = `V(${vertex.id})${addQuery}.next()`
-    return client.submit(`g.${query}`)
-  }
-
-  delete = (vertexId: number) => g.V(vertexId).drop().next()
-
-  drop = () => g.V().drop().next()
-
-  clean = () => client.submit(`g.V().not(bothE()).drop().next()`)
 }
 
-class EdgeQueries {
-  check = (edgeId: string) => g.E(edgeId).hasNext()
+class VertexQueries extends Utils {
+  get = async (properties: { [key: string]: string }) => {
+    const query = `V()${this.searchQuery(properties)}.elementMap().toList()`
+    const vertices = (await this.submit(query))._items
+    return vertices.map((vertex: any) => Object.fromEntries(vertex))
+  }
 
-  get = (id: string) => g.E(id).next()
+  add = async (properties: { [key: string]: string }) => {
+    const label = properties.label ? properties.label : 'vertex'
+    const query = `addV('${label}')${this.createAddQuery(properties)}.next()`
+    return (await this.submit(query))._items[0]
+  }
 
-  add = (source: number, target: number, label: string = 'superior') =>
-    client.submit(
-      `g.addE('${label}').from(V(${source})).to(V(${target})).next()`
-    )
+  update = async (properties: { [key: string]: string }) => {
+    const addQuery = this.createAddQuery(properties)
+    const query = `V(${properties.id})${addQuery}.next()`
+    return (await this.submit(query))._items
+  }
+
+  delete = async (id: number) => await g.V(id).drop().next()
+
+  drop = async () => await g.V().drop().next()
+}
+
+class EdgeQueries extends Utils {
+  #converter = (map: Map<string, any>) => {
+    const result: { [key: string]: any } = {}
+
+    for (const [key, value] of map.entries()) {
+      if (value instanceof Map) {
+        result[key] = this.#converter(value)
+      } else {
+        result[key] = value
+      }
+    }
+    return result
+  }
+
+  get = async (properties: { [key: string]: string }) => {
+    const query = `E()${this.searchQuery(properties)}.elementMap().toList()`
+    const response = (await this.submit(query))._items
+    return response.map((edge: any) => this.#converter(edge))
+  }
+
+  add = async (property: { [key: string]: string }) => {
+    const label = property.label ? property.label : 'out'
+    const query = `V(${property.from}).addE('${label}').to(V(${
+      property.to
+    }))${this.createAddQuery(property)}.next()`
+    return await this.submit(query)
+  }
 
   delete = (id: string) => g.E(id).drop().next()
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import EdgeController from './controllers/edgeController'
-import VertexController from './controllers/vertexController'
 import express, { Express, Request, Response } from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import gremlin from './gremlin'
 
 const app: Express = express()
 const port = process.env.PORT || 3000
@@ -10,54 +9,99 @@ const port = process.env.PORT || 3000
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 
+let globalRes: Response
+
+const validateBody = (body: { [key: string]: string }) => {
+  const final: { [key: string]: string } = {}
+  for (const key in body) {
+    if (body[key] !== undefined && body[key] !== null && body[key] !== '') {
+      final[key.toLowerCase()] = body[key].replace(/'/g, "\\'").toLowerCase()
+    }
+  }
+  return final
+}
+
+const safeQuery = async (query: Function) => {
+  try {
+    const response = await query()
+    globalRes.json(response)
+  } catch (error) {
+    console.error('Error executing Gremlin query:', error)
+    globalRes.status(500).json({ error: 'Something went wrong.' })
+  }
+}
+
+const validateData = (data: { [key: string]: string }) => {
+  if (Object.keys(data).length === 0) {
+    globalRes.status(400).json({ error: 'No data provided.' })
+    return false
+  }
+  return true
+}
+
 app.use((req, res, next) => {
   console.log(`${new Date().toISOString()}, ${req.method}, ${req.url}`)
+  globalRes = res
+  req.body = validateBody(req.body)
   next()
 })
 
-const vertex = VertexController
-const edge = EdgeController
-
 app
   .route('/vertex')
-  .get((req: Request, res: Response) => {
-    try {
-      vertex.getOne(res, req.body.id)
+  .get(async (req: Request, res: Response) => {
+    safeQuery(() => gremlin.vertex.get(req.body))
+  })
+  .post(async (req: Request, res: Response) => {
+    if (!validateData(req.body)) return
+    safeQuery(() => gremlin.vertex.add(req.body))
+  })
+  .put(async (req: Request, res: Response) => {
+    if (!validateData(req.body)) return
+    else if (req.body.id === undefined)
+      res.status(400).json({ error: 'No id provided.' })
+    else {
+      const vertices = await gremlin.vertex.get({ id: req.body.id })
+      if (Object.keys(vertices).length === 0)
+        res.status(404).json({ error: 'Vertex not found.' })
+      else safeQuery(() => gremlin.vertex.update(req.body))
     }
   })
-  .post((req: Request, res: Response) => {
-    console.log(req.body)
-    vertex.add(res, req.body)
-  })
-  .put((req: Request, res: Response) => {
-    vertex.update(res, req.body)
-  })
-  .delete((req: Request, res: Response) => {
-    vertex.delete(res, req.body.id)
+  .delete(async (req: Request, res: Response) => {
+    if (!validateData(req.body)) return
+    else if (!req.body.id) res.status(400).json({ error: 'No id provided.' })
+    else if (!Number(req.body.id))
+      res.status(400).json({ error: 'Invalid id provided.' })
+    else safeQuery(() => gremlin.vertex.delete(req.body.id))
   })
 
 app
   .route('/edge')
-  .get((req: Request, res: Response) => {
-    edge.getOne(res, req.body.id)
+  .get(async (req: Request, res: Response) => {
+    safeQuery(() => gremlin.edge.get(req.body))
   })
-  .post(async (req: Request, res: Response) => {
-    edge.add(res, req.body.from, req.body.to)
+  .post((req: Request, res: Response) => {
+    if (!validateData(req.body)) return
+    else if (!Number(req.body.from) || !Number(req.body.to))
+      res.status(400).json({ error: 'Invalid id provided.' })
+    else {
+      console.log('Body:', req.body)
+      safeQuery(() =>
+        Promise.all([
+          gremlin.vertex.get({ id: req.body.from }),
+          gremlin.vertex.get({ id: req.body.to }),
+        ]).then(async (values) => {
+          if (!values[0].length || !values[1].length)
+            return { error: 'Vertex(s) not found.' }
+          else await gremlin.edge.add(req.body)
+        })
+      )
+    }
   })
-  .delete((req: Request, res: Response) => {
-    edge.delete(res, req.body.id)
+  .delete(async (req: Request, res: Response) => {
+    if (!validateData(req.body)) return
+    safeQuery(() => gremlin.edge.delete(req.body.id))
   })
-
-app.delete('/clean', async (req: Request, res: Response) => {
-  request(res, await vertex.clean())
-})
-
-app.delete('/drop', async (req: Request, res: Response) => {
-  request(res, await vertex.drop())
-})
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`)
 })
-
-export default request
